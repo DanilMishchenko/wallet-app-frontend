8542c7cf92e5aa9abc12f0ac31f559d91cb482a0
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2022-07-30 14:14:32 +0300

    Create PrimaryButton.styled.jsx

---
+++ src/components/PrimaryButton/PrimaryButton.styled.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  display: block;
+  min-width: 300px;
+  height: 50px;
+  background-color: var(--green);
+  border-radius: 20px;
+  border: 0;
+  color: var(--primary-text);
+  font-size: 18px;
+  line-height: 27px;
+  text-align: center;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+`;
