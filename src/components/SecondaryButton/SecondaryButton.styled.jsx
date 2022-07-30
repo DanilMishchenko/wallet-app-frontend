8389e7ef80798fc5056160682a61bfe278d6c5db
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2022-07-30 14:23:47 +0300

    Create SecondaryButton.styled.jsx

---
+++ src/components/SecondaryButton/SecondaryButton.styled.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  display: block;
+  min-width: 300px;
+  height: 50px;
+  background-color: var(--primary-text);
+  border: 1px solid var(--blue);
+  border-radius: 20px;
+  color: var(--blue);
+  font-size: 18px;
+  line-height: 27px;
+  text-align: center;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+`;
