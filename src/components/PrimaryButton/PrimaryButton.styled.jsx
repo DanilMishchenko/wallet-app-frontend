25ed91c0fc43b535e64d4b1aa549f49b78b32c9b
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2022-07-30 14:21:02 +0300

    Update PrimaryButton.styled.jsx

--- src/components/PrimaryButton/PrimaryButton.styled.jsx
+++ src/components/PrimaryButton/PrimaryButton.styled.jsx
@@ -5,8 +5,8 @@ export const Button = styled.button`
   min-width: 300px;
   height: 50px;
   background-color: var(--green);
-  border-radius: 20px;
   border: 0;
+  border-radius: 20px;
   color: var(--primary-text);
   font-size: 18px;
   line-height: 27px;
