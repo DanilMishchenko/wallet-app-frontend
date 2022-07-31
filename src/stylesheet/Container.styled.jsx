23397859f37a9ff61d2ca12955000283adb9ccd9
OLgaPonomarenko <fblackberries@gmail.com>
2022-07-31 14:26:48 +0300

    clear

--- src/stylesheet/Container.styled.jsx
+++ src/stylesheet/Container.styled.jsx
@@ -3,11 +3,17 @@ import styled from 'styled-components';
 export const Container = styled.div`
   padding-right: 20px;
 
+  @media screen and (min-width: 320px) {
+    width: 320px;
+  }
+
   @media screen and (min-width: 768px) {
+    width: 768px;
     padding-right: 32px;
   }
 
   @media screen and (min-width: 1280px) {
+    width: 1280px;
     padding-right: 16px;
   }
 `;
