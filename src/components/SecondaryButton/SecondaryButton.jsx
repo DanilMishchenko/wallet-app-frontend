cff84ac128bbbaf72d2e9044545e5c45ec680077
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2022-07-31 16:42:59 +0300

    Update SecondaryButton.jsx

--- src/components/SecondaryButton/SecondaryButton.jsx
+++ src/components/SecondaryButton/SecondaryButton.jsx
@@ -11,5 +11,5 @@ export const SecondaryButton = ({ textBtn, onClick }) => {
 
 SecondaryButton.propTypes = {
   textBtn: PropTypes.string.isRequired,
-  onClick: PropTypes.func.isRequired,
+  onClick: PropTypes.func,
 };
