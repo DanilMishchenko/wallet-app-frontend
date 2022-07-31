0533dd47b851f3299c8f122a80eadab067265a84
OLgaPonomarenko <fblackberries@gmail.com>
2022-07-31 14:38:24 +0300

    replace blur background

--- src/components/layout/dashboard/Dashboard.jsx
+++ src/components/layout/dashboard/Dashboard.jsx
@@ -4,30 +4,11 @@ import styled from 'styled-components'
 import NavBar from '../navbar/NavBar'
 import Sidebar from '../sidebar/SideBar'
 import { Container } from '../../../stylesheet/Container.styled'
+import BlurBackground from './BlurBackground.styled'
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-`
-
-const BlurBackground = styled.div`
-  position: absolute;
-  z-index: -1;
-  width: 100%;
-  height: 100%;
-
-    @media (min-width: 768px) {
-      min-height: 1024px;
-      filter: blur(50px);  
-      background:
-        linear-gradient(rgba(210,210,210,0.2),rgba(255, 255, 255, 0.4)),
-        url(images/ellipse_pink.png) top right no-repeat,
-        url(images/ellipse_blue.png) bottom left  no-repeat;
-    };
-
-    @media (min-width: 1280px) {
-      min-height: calc(100vh - 105px);
-    };
 `
 
 const MainSection = styled.div`
