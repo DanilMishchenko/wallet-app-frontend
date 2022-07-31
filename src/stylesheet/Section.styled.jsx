97efa3d0851826f4b195bf9ffceb6d34e7121146
OLgaPonomarenko <fblackberries@gmail.com>
2022-07-31 18:57:45 +0300

    add blur

--- src/stylesheet/Section.styled.jsx
+++ src/stylesheet/Section.styled.jsx
@@ -1,34 +1,29 @@
 import styled from 'styled-components';
 import beigeShape1 from '../images/beigeShape1.svg';
-import beigeShape2 from '../images/beigeShape.svg';
 import purpleShape from '../images/purpleShape.svg';
 
 export const Section = styled.div`
   @media screen and (min-width: 768px) {
     height: 100vh;
     background-color: var(--background-color);
-    background-repeat: no-repeat;
-    background-image: url(${beigeShape2});
-    background-position: top 0px right 0px;
 
+      ::before {
+        content: '';
+        position: absolute;
+        display: block;
+        top: 1px;
+        width: 100%;
+        height: 100%;
+        background-image: url(${purpleShape}), url(${beigeShape1});
+        background-position: bottom -50px left 0px, top -150px right -50px;
+        background-repeat: no-repeat, no-repeat;
+        filter: blur(50px)
+      }
+  };
+
+  @media screen and (min-width: 1080px) {
     ::before {
-      content: '';
-      position: absolute;
-      display: block;
-      top: 1px;
-      width: 100%;
-      height: 100%;
-      background-repeat: no-repeat;
-      background-image: url(${purpleShape});
-      background-position: bottom 0px left 0px;
-    }
-  }
-
-  @media screen and (min-width: 1280px) {
-    background-image: url(${beigeShape1});
-
-    ::before {
-      background-image: url(${purpleShape});
+      background-position: bottom 0px left 0px, top 0px right 0px;
     }
   }
 `;
