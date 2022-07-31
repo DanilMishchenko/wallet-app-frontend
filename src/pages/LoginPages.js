933e0ea46e874fc64bf2caffb2b3afc5e6090c07
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2022-07-31 18:21:17 +0300

    Update LoginPages.js

--- src/pages/LoginPages.js
+++ src/pages/LoginPages.js
@@ -1,7 +1,9 @@
+import { LoginForm } from '../components/LoginForm/LoginForm';
+
 export default function LoginPages() {
   return (
     <>
-      <h1>LoginPages</h1>
+      <LoginForm />
     </>
   );
 }
