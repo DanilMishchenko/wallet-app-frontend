e75290f9bef89951b99269c6d5c013c8775690c4
OLgaPonomarenko <fblackberries@gmail.com>
2022-08-01 22:15:09 +0300

    currency style to module

--- src/components/Currency/Currency.styled.jsx
+++ src/components/Currency/Currency.styled.jsx
@@ -21,3 +21,41 @@ export const CurrencyLayout = styled.div`
     height: 347px;
   } ;
 `;
+
+export const TableContainer = styled.table`
+  width: 100%;
+  border-spacing: 0;
+  color: var(--primary-text);
+  line-height: 1.43;
+`;
+
+export const TableHead = styled.thead`
+  color: var(--primary-text);
+  background: rgba(255, 255, 255, 0.2);
+  font-size: 18px;
+  text-transform: capitalize;
+  text-align: center;
+`;
+
+export const TableHeadText = styled.td`
+  padding: 10px 0px;
+  min-width: 70px;
+  @media (min-width: 768px) {
+    padding: 16px 0px;
+  } ;
+`;
+
+export const TableRow = styled.tr`
+  font-size: 16px;
+`;
+
+export const TableData = styled.td`
+  text-align: center;
+  padding-top: 12px;
+  @media (min-width: 768px) {
+    padding-top: 12px;
+  }
+  @media (min-width: 1280px) {
+    padding-top: 24px;
+  } ;
+`;
