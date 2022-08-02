e4de75e5eba3346dc6071be010a0f7dffb136655
Lana Ovcharenko <lana.ovchar@gmail.com>
2022-08-02 19:55:19 +0300

    add chart diagram

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,7 +1,43 @@
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+import { Container } from './Chart.styled';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 export const Chart = () => {
+  const data = {
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [8700, 3800.74, 1500, 800, 2208.5, 300, 3400, 1230, 610],
+        backgroundColor: [
+          '#FED057',
+          '#FFD8D0',
+          '#FD9498',
+          '#C5BAFF',
+          '#6E78E8',
+          '#4A56E2',
+          '#81E1FF',
+          '#24CCA7',
+          '#00AD84',
+        ],
+      },
+    ],
+  };
+
+  // const options = {
+  //   maintainAspectRatio: false,
+  //   scales: {},
+  //   legend: {
+  //     labels: {
+  //       fontSize: 25,
+  //     },
+  //   },
+  // };
+
   return (
-    <>
-      <h1>Chart</h1>
-    </>
+    <Container>
+      <Doughnut data={data} />
+    </Container>
   );
 };
---
+++ src/components/Chart/Chart.styled.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  @media screen and (min-width: 320px) {
+    width: 280px;
+    height: 280px;
+  }
+`;
