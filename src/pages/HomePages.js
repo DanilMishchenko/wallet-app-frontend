1ca79fde3aaadda772d68276045a8fbd6dd984a6
igormos1 <gior_gt@ukr.net>
2022-08-05 17:03:25 +0300

    fixprob

--- src/pages/HomePages.js
+++ src/pages/HomePages.js
@@ -32,8 +32,6 @@ export default function HomePages() {
       <ButtonAddTransactions
         onClick={() => dispatch(globalActions.openModalAddTransaction())}
       />
-
-      {/* {ModalTransitionOpen && <ModalAddTransaction onModal={onModalClose} />} */}
     </>
   );
 }
