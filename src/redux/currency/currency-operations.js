bba7d03a734ca5e0cd39e755a5180fe0a5c0cf1a
OLgaPonomarenko <fblackberries@gmail.com>
2022-08-06 02:27:57 +0300

    +

--- src/redux/currency/currency-operations.js
+++ src/redux/currency/currency-operations.js
@@ -1,7 +1,8 @@
 //import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-const CURRENCY_URI = `https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5`;
+const CURRENCY_URI =
+  'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5';
 
 const getCurrency = createAsyncThunk('currency/fetch', async () => {
   const res = await fetch(CURRENCY_URI).then(data => data.json());
