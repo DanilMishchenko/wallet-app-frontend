6f94cf9a58e3b9b3660dbd886ab24f663190b3e6
OLgaPonomarenko <fblackberries@gmail.com>
2022-08-07 00:24:31 +0300

    add balance data

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,13 +1,25 @@
+import { useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { BalanceBoard, HeadText, Content, Symbol } from './Balance.styled';
+import useMediaQuery from '../../hooks/useMediaQuery';
 
 export const Balance = () => {
+  const { pathname } = useLocation();
+  const balance = useSelector(state => state.balance.value);
+  const isMobile = useMediaQuery('(max-width: 768px)');
+  const isBalanceShow = isMobile ? pathname === '/home' : true;
+
   return (
-    <BalanceBoard>
-      <HeadText>Your balance</HeadText>
-      <Content>
-        <Symbol>₴</Symbol>
-        <p>24 000.00</p>
-      </Content>
-    </BalanceBoard>
+    <>
+      {isBalanceShow && (
+        <BalanceBoard>
+          <HeadText>Your balance</HeadText>
+          <Content>
+            <Symbol>₴</Symbol>
+            <p>{balance}</p>
+          </Content>
+        </BalanceBoard>
+      )}
+    </>
   );
 };
