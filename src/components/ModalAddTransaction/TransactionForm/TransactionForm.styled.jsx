f2a8948da5dc9ef2c176d7dbf08e88bb229f601d
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2022-08-09 20:35:05 +0300

    Create TransactionForm.styled.jsx

---
+++ src/components/ModalAddTransaction/TransactionForm/TransactionForm.styled.jsx
@@ -0,0 +1,182 @@
+import styled from 'styled-components';
+import { Field, ErrorMessage } from 'formik';
+
+export const SelectContainer = styled.div`
+  /* display: flex;
+  align-items: center;
+  position: relative; */
+  position: relative;
+  margin-bottom: 40px;
+
+  /* ::after {
+    content: '';
+    position: absolute;
+    display: block;
+    bottom: 0;
+    height: 1px;
+    width: 100%;
+    background-color: #bdbdbd;
+  } */
+
+  @media screen and (min-width: 768px) {
+    margin-bottom: 40px;
+  }
+`;
+
+export const ArrowSvg = styled.img`
+  position: absolute;
+  pointer-events: none;
+  right: 20px;
+  width: 18px;
+  height: 9px;
+`;
+
+export const FieldSelect = styled(Field)`
+  width: 100%;
+  height: 34px;
+  appearance: none;
+  border: none;
+  outline: none;
+  padding-left: 20px;
+
+  option {
+    border: none;
+    border-radius: 5px;
+  }
+`;
+
+export const Wrapper = styled.div`
+  @media screen and (min-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+export const InputSum = styled.input`
+  display: block;
+  padding-left: 20px;
+  width: 100%;
+  height: 34px;
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 1.5;
+  border: none;
+  cursor: pointer;
+  outline: none;
+
+  ::placeholder {
+    color: #bdbdbd;
+  }
+  @media screen and (min-width: 768px) {
+    padding-left: 0px;
+    text-align: center;
+  }
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+  width: 100%;
+  margin-bottom: 40px;
+
+  ::after {
+    content: '';
+    position: absolute;
+    display: block;
+    bottom: 0;
+    height: 1px;
+    width: 100%;
+    background-color: #bdbdbd;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 181px;
+  }
+`;
+
+export const TextAreaComment = styled.textarea`
+  position: relative;
+  display: block;
+  padding-left: 20px;
+  width: 100%;
+  resize: none;
+  border: none;
+  font-size: 18px;
+  font-weight: 400;
+  outline: none;
+  height: 4em;
+  resize: inherit;
+
+  ::placeholder {
+    color: #bdbdbd;
+  }
+
+  @media screen and (min-width: 768px) {
+    height: 2.3em;
+  }
+`;
+
+export const DataContainer = styled.div`
+  display: flex;
+  position: relative;
+  margin-bottom: 40px;
+
+  ::after {
+    content: '';
+    position: absolute;
+    display: block;
+    bottom: 0;
+    height: 1px;
+    width: 100%;
+    background-color: #bdbdbd;
+  }
+
+  div.input {
+    outline: none;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 181px;
+  }
+`;
+
+export const InputDate = styled.input`
+  display: block;
+  width: 100%;
+  height: 34px;
+  padding-left: 20px;
+  font-size: 18px;
+  font-weight: 400;
+  border: none;
+  cursor: pointer;
+  outline: none;
+
+  @media screen and (min-width: 768px) {
+    padding-left: 8px;
+  }
+`;
+
+export const CommentContainer = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+  width: 100%;
+  margin-bottom: 40px;
+
+  ::after {
+    content: '';
+    position: absolute;
+    display: block;
+    bottom: 0;
+    height: 1px;
+    width: 100%;
+    background-color: #bdbdbd;
+  }
+`;
+
+export const Error = styled(ErrorMessage)`
+  color: '#FF6596';
+  position: 'absolute';
+  top: '130%';
+`;
