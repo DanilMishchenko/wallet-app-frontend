8453368ddcfea89a7170ee064111f2ba7fb3fcd9
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2022-08-09 20:34:56 +0300

    Update ModalAddTransaction.jsx

--- src/components/ModalAddTransaction/ModalAddTransaction.jsx
+++ src/components/ModalAddTransaction/ModalAddTransaction.jsx
@@ -15,8 +15,8 @@ import {
 } from './ModalAddTransition.styled';
 import plusIcon from '../../images/plusIcon.svg';
 import minusIcon from '../../images/minusIcon.svg';
-import { IncomeForm } from './IncomeForm/IncomeForm';
-import { ExpenseForm } from './ExpenceForm/ExpenseForm';
+import { TransactionForm } from './TransactionForm/TransactionForm';
+//import { ExpenseForm } from './ExpenceForm/ExpenseForm';
 import { SecondaryButton } from '../SecondaryButton/SecondaryButton';
 import { fetchTransactions } from '../../redux/transactions/transactions-operations';
 import closeModal from '../../images/closeModal.svg';
@@ -26,6 +26,23 @@ const modalRoot = document.querySelector('#modal-transaction');
 export const ModalAddTransaction = ({ onModal }) => {
   const dispatch = useDispatch();
   const [checked, setChecked] = useState(true);
+
+  const IncomeOptions = [
+    { value: 'Regular Income', label: 'Regular Income' },
+    { value: 'Irregular Income', label: 'Irregular Income' },
+  ];
+
+  const ExpenseOptions = [
+    { value: 'Basic expenses', label: 'Basic expenses' },
+    { value: 'Products', label: 'Products' },
+    { value: 'Car', label: 'Car' },
+    { value: 'Self care', label: 'Self care' },
+    { value: 'Child care', label: 'Child care' },
+    { value: 'Household products', label: 'Household products' },
+    { value: 'Education', label: 'Education' },
+    { value: 'Other expenses', label: 'Other expenses' },
+    { value: 'Leisure', label: 'Leisure' },
+  ];
 
   useEffect(() => {
     const handleKeyDown = e => {
@@ -109,11 +126,16 @@ export const ModalAddTransaction = ({ onModal }) => {
           <ExpenseTrigger checked={checked}>Expense</ExpenseTrigger>
         </Container>
         <FormContainer>
-          {!checked ? (
-            <IncomeForm onClose={onModal} />
-          ) : (
-            <ExpenseForm onClose={onModal} />
-          )}
+          <TransactionForm
+            initialValues={{
+              type: checked ? true : false,
+              category: '',
+              sum: '',
+              date: new Date(),
+            }}
+            onClose={onModal}
+            options={!checked ? IncomeOptions : ExpenseOptions}
+          />
         </FormContainer>
         <SecondaryButton textBtn="cancel" onClick={() => onModal()} />
       </Wrapper>
@@ -121,3 +143,127 @@ export const ModalAddTransaction = ({ onModal }) => {
     modalRoot,
   );
 };
+
+// import { createPortal } from 'react-dom';
+// import { useEffect, useState } from 'react';
+// import Switch from 'react-switch';
+// import { useDispatch } from 'react-redux';
+// import {
+//   Overlay,
+//   Container,
+//   Title,
+//   IncomeTrigger,
+//   ExpenseTrigger,
+//   FormContainer,
+//   SwitchContainer,
+//   Wrapper,
+//   CloseButton,
+// } from './ModalAddTransition.styled';
+// import plusIcon from '../../images/plusIcon.svg';
+// import minusIcon from '../../images/minusIcon.svg';
+// import { IncomeForm } from './IncomeForm/IncomeForm';
+// import { ExpenseForm } from './ExpenceForm/ExpenseForm';
+// import { SecondaryButton } from '../SecondaryButton/SecondaryButton';
+// import { fetchTransactions } from '../../redux/transactions/transactions-operations';
+// import closeModal from '../../images/closeModal.svg';
+
+// const modalRoot = document.querySelector('#modal-transaction');
+
+// export const ModalAddTransaction = ({ onModal }) => {
+//   const dispatch = useDispatch();
+//   const [checked, setChecked] = useState(true);
+
+//   useEffect(() => {
+//     const handleKeyDown = e => {
+//       if (e.code === 'Escape') {
+//         onModal();
+//       }
+//     };
+//     window.addEventListener('keydown', handleKeyDown);
+//     return () => {
+//       dispatch(fetchTransactions());
+//       window.removeEventListener('keydown', handleKeyDown);
+//     };
+//   }, [dispatch, onModal]);
+
+//   const handleBackdropClick = event => {
+//     if (event.currentTarget === event.target) {
+//       onModal();
+//     }
+//   };
+
+//   const handleChange = nextChecked => {
+//     setChecked(nextChecked);
+//   };
+
+//   return createPortal(
+//     <Overlay onClick={handleBackdropClick}>
+//       <Wrapper>
+//         <CloseButton type="button" onClick={() => onModal()}>
+//           <img src={closeModal} alt="closeModal" />
+//         </CloseButton>
+//         <Title>Add transaction</Title>
+//         <Container>
+//           <IncomeTrigger checked={checked}>Income</IncomeTrigger>
+//           <SwitchContainer>
+//             <Switch
+//               checked={checked}
+//               onChange={handleChange}
+//               handleDiameter={44}
+//               offColor="#E0E0E0"
+//               onColor="#E0E0E0"
+//               offHandleColor="#24CCA7"
+//               onHandleColor="#FF6596"
+//               activeBoxShadow="none"
+//               height={40}
+//               width={80}
+//               borderRadius={30}
+//               uncheckedIcon={false}
+//               checkedIcon={false}
+//               uncheckedHandleIcon={
+//                 <div
+//                   style={{
+//                     display: 'flex',
+//                     justifyContent: 'center',
+//                     alignItems: 'center',
+//                     height: '100%',
+//                     backgroundColor: 'transparent',
+//                     borderRadius: '30px',
+//                     boxShadow: '0px 6px 15px rgba(36, 204, 167, 0.5)',
+//                   }}
+//                 >
+//                   <img src={plusIcon} alt="plus" />
+//                 </div>
+//               }
+//               checkedHandleIcon={
+//                 <div
+//                   style={{
+//                     display: 'flex',
+//                     justifyContent: 'center',
+//                     alignItems: 'center',
+//                     height: '100%',
+//                     backgroundColor: 'transparent',
+//                     borderRadius: '30px',
+//                     boxShadow: '0px 6px 15px rgba(255, 101, 150, 0.5)',
+//                   }}
+//                 >
+//                   <img src={minusIcon} alt="minus" />
+//                 </div>
+//               }
+//             />
+//           </SwitchContainer>
+//           <ExpenseTrigger checked={checked}>Expense</ExpenseTrigger>
+//         </Container>
+//         <FormContainer>
+//           {!checked ? (
+//             <IncomeForm onClose={onModal} />
+//           ) : (
+//             <ExpenseForm onClose={onModal} />
+//           )}
+//         </FormContainer>
+//         <SecondaryButton textBtn="cancel" onClick={() => onModal()} />
+//       </Wrapper>
+//     </Overlay>,
+//     modalRoot,
+//   );
+// };
